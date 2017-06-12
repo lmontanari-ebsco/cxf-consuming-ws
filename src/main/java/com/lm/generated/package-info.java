9cc02ef823bfc62cfb1c0e4cc5fda00e0d7b848e
Lucas Montanari <lucas_montanari@hotmail.com>
2017-06-12 11:34:03 -0300

    Removing generated classes

--- src/main/java/com/lm/generated/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:accounting_2016_1.lists.webservices.netsuite.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.lm.generated;
